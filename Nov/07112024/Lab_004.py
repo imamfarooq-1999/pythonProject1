fbb710a90da063bad396f4e976e70a7c7e87238b
farukh <t.imamfarooq@gmail.com>
2024-11-14 13:26:28 +0100

    7 nov

---
+++ Nov/07112024/Lab_004.py
@@ -0,0 +1,14 @@
+#how to take the user input
+
+num1 = int(input("Enter the num1"+" "))
+num2 = int(input("Enter the num2"+" "))
+
+sum = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("sum is :",num1 + num2)
+print("sub is :",num1 - num2)
+print("mul is :",num1 * num2)
+print("div is :",num1 / num2)
---
+++ Nov/Lab_003.py
@@ -0,0 +1,30 @@
+#hoe to take the user input
+
+
+
+num1 = input("enter the first value1"+" ")
+num2 = input("enter the first value2"+" ")
+num3 = input("enter the first value3"+" ")
+
+num1 = int(num1)
+num2 = int(num2)
+num3 = int(num3)
+
+print(num1+num2+num3)
+print(num1-num2-num3)
+print(num1*num2*num3)
+print(num1/num2/num3)
+
+
+
+a = 15
+b = 2
+
+c = a // b
+d  = a % b
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print("Enter the quotient " + " " ,c)
+print("Enter the remainder " + " " ,d)
