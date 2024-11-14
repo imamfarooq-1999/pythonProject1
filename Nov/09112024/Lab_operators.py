474ea13f039a46a4dddbfa71524ec231c442c954
farukh <t.imamfarooq@gmail.com>
2024-11-14 15:15:09 +0100

    9 nov

---
+++ Nov/09112024/Lab_operators.py
@@ -0,0 +1,56 @@
+#uinary operator
+"""
+age = +65
+print(age)
+
+age1  = -65+1
+
+print(age1)"""
+from itertools import filterfalse
+
+#Assignment operator
+"""
+print(5/2)
+print(5//2)
+
+print(5%2)
+
+is_pramod_married= True
+print(not is_pramod_married)
+print(is_pramod_married)
+
+print(2 == 2)
+print (2 == 3)"""
+
+#Logical operator
+
+"""
+x = 10
+y = 20
+print(x > y)
+print(x < y)"""
+
+"""x = 10
+y = 20
+print(x <= 20)
+
+print (y>= x)
+"""
+#Or or And Operator
+
+"""f = False
+t = True
+
+print (f or t)
+print (f and t)
+
+x = 10
+y = 20
+result = ( x != y)
+print(result)"""
+
+# Divmod Operator
+
+a, b = divmod(15,2)
+print (a)
+print (b)
