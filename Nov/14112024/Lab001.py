fbbe44005029434ac5f2bf007994a1b97df398a3
farukh <t.imamfarooq@gmail.com>
2024-11-17 07:00:55 +0100

    17 nov

---
+++ Nov/14112024/Lab001.py
@@ -0,0 +1,13 @@
+# if elif else condition
+
+
+score = int(input("Enter the score \n"))
+
+if score >= 90 and score <= 100:
+    print("your grade is --->" "A")
+elif score >= 75 and score <= 90:
+    print("your grade is ---->" "B")
+elif score >= 60 and score <=75:
+    print("your grade is --->"" first class")
+else :
+    print ("Fail")
