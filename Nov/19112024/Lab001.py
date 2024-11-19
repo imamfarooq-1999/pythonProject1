2f1869513ade306815ece0d98310dc54e9144ef5
farukh <t.imamfarooq@gmail.com>
2024-11-19 16:54:33 +0100

    17 nov. 19 nov.

--- Nov/19112024/Lab001.py
+++ Nov/19112024/Lab001.py
@@ -5,6 +5,7 @@ from idlelib.debugger_r import restart_subprocess_debugger
 # result = mul_args()
 # print(result)
 
+"""
 num1 = input("enter the value1\n")
 num2 = input("enter the value1\n")
 num3 = input("enter the value1\n")
@@ -18,4 +19,35 @@ result = sum_of_the_values(num1=10, num2=10, num3=10)
 print(result)
 
 result1 = sum_of_the_values()
-print(result1)
+print(result1)"""
+
+"""
+def args_1(*args):
+    return args
+result = args_1(str("hello"))
+print(result)"""
+
+
+"""
+def multiple_args(name1 = "a", name2="b"):
+    print("Hello", name1, name2)
+
+multiple_args()
+"""
+
+
+"""
+def mul_args(name1='a', name2='b'):
+    print("mul ->", name1, name2)
+mul_args("pramodh","amit")
+mul_args(name1="pramodh")
+mul_args(name2='pramodh')
+mul_args(name2='amit',name1='pramodh')"""
+
+def funtion_of_funtion():
+    print('world')
+
+def fun1():
+    print("hello")
+    funtion_of_funtion()
+fun1()
